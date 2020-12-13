4e67bb0018bc219e47e6947c9ff05e152d1d6b43
Aakash Verma <aakashvermaee@gmail.com>
2020-12-13 20:02:15 +0530

    add ISO dates in logger

--- src/app/shared/services/logger.service.ts
+++ src/app/shared/services/logger.service.ts
@@ -21,25 +21,41 @@ export class LoggerService {
 
   info(message: any): void {
     if (!this.canExecute()) {
-      console.log(`%c${LOG_TYPE.INFO.type}`, `color: ${LOG_TYPE.INFO.color}`, message);
+      console.log(
+        `%c[${LOG_TYPE.INFO.type} ${new Date().toISOString()}]`,
+        `color: ${LOG_TYPE.INFO.color}`,
+        message
+      );
     }
   }
 
   debug(message: any): void {
     if (!this.canExecute()) {
-      console.log(`%c${LOG_TYPE.DEBUG.type}`, `color: ${LOG_TYPE.DEBUG.color}`, message);
+      console.log(
+        `%c[${LOG_TYPE.DEBUG.type} ${new Date().toISOString()}]`,
+        `color: ${LOG_TYPE.DEBUG.color}`,
+        message
+      );
     }
   }
 
   error(message: any): void {
     if (!this.canExecute()) {
-      console.log(`%c${LOG_TYPE.ERROR.type}`, `color: ${LOG_TYPE.ERROR.color}`, message);
+      console.log(
+        `%c[${LOG_TYPE.ERROR.type} ${new Date().toISOString()}]`,
+        `color: ${LOG_TYPE.ERROR.color}`,
+        message
+      );
     }
   }
 
   warning(message: any): void {
     if (!this.canExecute()) {
-      console.log(`%c${LOG_TYPE.WARNING.type}`, `color: ${LOG_TYPE.WARNING.color}`, message);
+      console.log(
+        `%c[${LOG_TYPE.WARNING.type} ${new Date().toISOString()}]`,
+        `color: ${LOG_TYPE.WARNING.color}`,
+        message
+      );
     }
   }
 }
