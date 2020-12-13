84ac123651f9a22440527e443ec72367580cb8c3
Aakash Verma <aakashvermaee@gmail.com>
2020-12-13 15:55:44 +0530

    code refactor

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,5 @@
 export * from './components/not-found/not-found.component';
 export * from './services/logger.service';
 export * from './services/local-storage.service';
+export * from './constants/app-constants';
+export * from './types/IDeserializable';
