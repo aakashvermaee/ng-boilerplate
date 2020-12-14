36aacb632ed42b4d753b2ef7470f663635ebfb64
aakashvermaee <aakashvermaee@gmail.com>
2020-12-14 17:41:53 +0530

    added regex & auth props in app-constants

--- src/app/shared/constants/app-constants.ts
+++ src/app/shared/constants/app-constants.ts
@@ -1,3 +1,11 @@
 export const APP_CONSTANTS = {
+  regex: {
+    passwordRegex: /^(?=.{8,})(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+*!=]).*$/,
+  },
+  auth: {
+    // headers
+    accessToken: 'x-access-token',
+    refreshToken: 'x-refresh-token',
+  },
   PRELOAD_ASSETS: [],
 };
